ede75b2f61990d898b87a1f266c47448fced4020
NitinVerma22 <132615638+NitinVerma22@users.noreply.github.com>
2024-01-22 00:21:37 +0530

    Create script.js

---
+++ LandingPage/script.js
@@ -0,0 +1,14 @@
+function hel(){
+setInterval(function()  {
+    document.getElementById("bg").style.backgroundImage = "url(/img/bg2.jpeg)";
+}, 3000);
+setInterval(function()  {
+    document.getElementById("bg").style.backgroundImage = "url(/img/bg3.jpeg)";
+}, 6000);
+setInterval(function()  {
+    document.getElementById("bg").style.backgroundImage = "url(/img/bacgroundimg1.jpeg)";
+}, 9000);
+
+}
+
+  document.getElementById("myDIV").styl
